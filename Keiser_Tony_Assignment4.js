a227814ef46ca49034be6bad8130742a0a568c03
keiser <keiser@fullsail.edu>
2013-02-26 22:08:42 -0700

    Project 4
    Check Email

--- Keiser_Tony_Assignment4.js
+++ Keiser_Tony_Assignment4.js
@@ -1,10 +1,13 @@
 //Project 4
 //Tony Keiser
 
+//Global Var
+
+var say = console.log;
+
 
 //Check Phone Number 
 
-var say = console.log;
 
 function phoneNumber(input){ // Index: 012-345-6789
 	
@@ -15,7 +18,30 @@ function phoneNumber(input){ // Index: 012-345-6789
 		if(input.length<10){
 			say("Please enter a 10 digit value. Sample: 123-456-7890");
 			} else {
-				say("You have entered the phone number value of: 1-" + areaCode + "-" + prefix + "-" + lineNumber);
+				say("Your Phone Number is: 1-" + areaCode + "-" + prefix + "-" + lineNumber);
 	};
 };
 phoneNumber("1234567890");
+
+//Check Email 
+
+function email(input){
+	
+	var start = input.indexOf('@'),
+		end = input.indexOf('.'),
+		at = input.charAt(start),
+		dot = input.charAt(end),
+		userName = input.substring(0, start),		
+		siteName = input.substring(start+1,end),
+		domain = input.substring(end+1);
+		
+	    
+
+	   
+		    if(start==-1 || end==-1){
+		  		  say("Invalid format. Format: joe@someone.com")
+		    } else {
+			    say("Your E-mail address is: " + userName + at + siteName + dot + domain);
+		    }
+};
+email("joe@someone.com");
